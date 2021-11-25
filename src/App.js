69cf9843bce64d0bfa1b92912647bb4ea03299af
Bengt <bengt.mansson@gmail.com>
2021-11-25 17:53:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+//import './App.css';
+
+
+function MyComponent() {
+  const numbers = useState([]);
+
+  fetch("/numbers.json")
+    .then((resp) => resp.json())
+    .then((data) => (numbers[0] = data));
+
+  return <div>
+    <div>Numbers: {JSON.stringify(numbers[0])}</div>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <MyComponent />
     </div>
   );
 }
